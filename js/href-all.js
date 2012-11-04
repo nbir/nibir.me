86ba294d7062d48461182fd801c7ddfc363072e5
Nibir Bora <nibirbora@gmail.com>
2012-11-03 23:12:45 -0700

    Upload ready

--- js/href-all.js
+++ js/href-all.js
@@ -1,6 +1,15 @@
- var BASE_URL = '//127.0.0.1:8020/nibir.me/';
+/* nibir.me - Nibir Bora
+ *
+ * Copyright (C) Nibir Bora
+ * Author: Nibir Bora <nbora@usc.edu>
+ * URL: <http://nibir.me/>
+ * For license information, see LICENSE
+ */
+
+
+var BASE_URL = '//127.0.0.1:8020/nibir.me/';
  
- $(document).ready(function() {
+$(document).ready(function() {
 	$('#a-home').attr('href', BASE_URL);
 	
 	$('#a-project-all').attr('href', BASE_URL+"projects/");
@@ -9,13 +18,14 @@
 	$('#a-project-etola').attr('href', BASE_URL+"projects/etola/");
 	$('#a-project-mejaj').attr('href', BASE_URL+"projects/mejaj/");
 	$('#a-project-ssms').attr('href', BASE_URL+"projects/ssms/");
-	$('#a-project-others').attr('href', BASE_URL+"projects/others/");
+	$('#a-project-others').attr('href', BASE_URL+"projects/others.html");
 	
 	$('#a-writing').attr('href', BASE_URL+"writing/");
 	$('#a-calendar').attr('href', BASE_URL+"calendar/");
 	$('#a-about').attr('href', BASE_URL+"about/");
 	
 	$('#a-cpl').attr('href', BASE_URL+"_cpl/");
+	$('#a-cpl').removeAttr("href")
 	
 	$('#a-resume').attr('href', BASE_URL+"~files/Nibir_Bora_(resume).pdf");
 	
@@ -23,4 +33,5 @@
 	$('#b-home').attr('href', BASE_URL);
 	
 	$('#b-projects').attr('href', BASE_URL+"projects/");
+	
 });
---
+++ projects/~files/projects_cms_Readme.txt
@@ -0,0 +1,7 @@
+-------------------------
+         README
+-------------------------
+
+Run the program in DOS Prompt.
+
+Run the Class named 'CompanyMenu'.
